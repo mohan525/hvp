6abcbcb54be0f737656e3589d1ffbb9e230cc88f
mohan525 <118957445+mohan525@users.noreply.github.com>
2022-12-15 17:11:23 +0530

    Update form.js

---
+++ form.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import "./form.css";
+
+const Form = () => {
+  return (
+     
+        <div className="vendor-det">
+      
+    <div className="form-div">
+    <div className="frame-div3">
+          <div className="label-div1">
+            <label className="country-label">Finance  <br></br>
+                Country*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">Country*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">Country*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">State*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">State*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          
+        </div>
+        <div className="frame-div3">
+          <div className="label-div1">
+            <label className="country-label">             <br></br>designaton*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">designaton*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">designaton*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">designaton*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">designaton*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+        </div>
+        <div className="frame-div3">
+          <div className="label-div1">
+            <label className="country-label">Phone no*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">Phone no*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">Phone no*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">Phone no*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">Phone no*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          
+        </div>
+        <div className="frame-div3">
+          <div className="label-div1">
+            <label className="country-label">email*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">email*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">email*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">email*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+          <div className="label-div1">
+            <label className="country-label">email*</label>
+            <input className="rectangle-input2" type="text" required />
+          </div>
+        </div>
+      </div>
+      <div className="basicinformation-div9">
+        <label className="basic-information-label">Communication details</label>
+      </div>
+      <div className="button-div10">
+        <div className="buttonrow-div11">
+          <button className="buttonstyle" value ="cancel">
+            <div className="cancel-div">Cancel</div>
+          </button>
+          <button className="buttonstyle" value="save">
+            <div className="cancel-div">Save</div>
+          </button>
+          <button className="buttonstyle" value="next">
+            <div className="cancel-div">Next</div>
+          </button>
+        </div>
+      </div>
+    </div>
+    
+    
+  )
+};
+
+export default Form;
